33beb0e7c9d589317611c9dcf23d18704d3c21be
Aimmatul Azra <47385022+aimmatulazra@users.noreply.github.com>
2019-04-04 21:23:28 +0700

    Add files via upload

---
+++ ini.js
@@ -0,0 +1,8 @@
+$('button').on('click',function{
+  var userName = $('#name').val();
+  var userAge = $('#age').val();
+  
+  console.log('Name: ' + userName\n'Umur: ' + userAge);
+  
+  event.preventDefault
+  });
